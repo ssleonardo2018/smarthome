865accd44a899f131e06862cfd53a05be3378d96
ssleonardo2018 <115373834+ssleonardo2018@users.noreply.github.com>
2026-04-09 16:25:26 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-function ligarBomba() {
-    alert("Bomba acionada com sucesso!");
-}
+let estado = false; // false = desligado | true = ligado
+
+const botao = document.getElementById("btnBomba");
+
+botao.addEventListener("click", () => {
+    estado = !estado;
+
+    if (estado) {
+        botao.classList.remove("desligado");
+        botao.classList.add("ligado");
+        botao.innerText = "Ligado";
+    } else {
+        botao.classList.remove("ligado");
+        botao.classList.add("desligado");
+        botao.innerText = "Desligado";
+    }
+
+    console.log("Bomba:", estado ? "Ligada" : "Desligada");
+});
